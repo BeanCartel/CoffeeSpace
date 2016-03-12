3c5dd667811abce781a781ea26a9d1c86ee57728
supremekhaoz <luisliz2038@gmail.com>
2016-03-12 17:02:03 -0400

    Ability to post new shops

--- CoffeeSpace/GlobalFuncs.swift
+++ CoffeeSpace/GlobalFuncs.swift
@@ -29,4 +29,16 @@ class GlobalFuncs: NSObject {
         }
         return nil
     }
+    
+    class func resizeImg(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRectMake(0, 0, newSize.width, newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
