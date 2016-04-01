9acb86e35e0269c5cab3bc56b76b82c386db2826
lanista38 <jorge.cruz7@upr.edu>
2016-04-01 15:45:42 -0400

    Revert "1"
    This reverts commit f485050ad8da89431eee02695a5dbabd7a0aac61.
    
    Signed-off-by: lanista38 <jorge.cruz7@upr.edu>

---
+++ CoffeeSpace/AvailableCoffeeCellCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AvailableCoffeeCellCollectionViewCell.swift
+//  CoffeeSpace
+//
+//  Created by Jorge Cruz on 4/1/16.
+//  Copyright © 2016 Jorge Cruz. All rights reserved.
+//
+
+import UIKit
+
+class AvailableCoffeeCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var brandImageView: UIImageView!
+    
+    @IBOutlet weak var brandTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 8
+        self.clipsToBounds = true
+        self.backgroundView?.alpha = 0.5
+        
+    }
+}
---
+++ CoffeeSpace/CoffeeShopCollectionViewController.swift
@@ -0,0 +1,80 @@
+//
+//  CoffeeShopCollectionViewController.swift
+//  CoffeeSpace
+//
+//  Created by Jorge Cruz on 3/31/16.
+//  Copyright © 2016 Jorge Cruz. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+
+class CoffeeShopCollectionViewController: UICollectionViewController {
+
+    @IBOutlet var collectionview: UICollectionView!
+    
+    var shopObject : PFObject?
+    var coffeeBrands: [PFObject]?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        coffeeBrands = shopObject?.valueForKey("availableCoffee") as? [PFObject]
+       
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return coffeeBrands!.count
+    }
+    
+   override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("AvailableCoffeeCellCollectionViewCell", forIndexPath: indexPath) as! AvailableCoffeeCellCollectionViewCell
+        print(shopObject?.valueForKey("shopName"))
+        print(coffeeBrands![0].valueForKey("objectId") )
+    
+        for p in coffeeBrands!{
+        let id = p.valueForKey("objectId") as? String
+        //let query = PFQuery(className: "coffeeBrand")
+        let brand = PFObject(outDataWithClassName: "coffeeBrand", objectId: id)
+           
+            do {
+                try brand.fetchIfNeeded()
+            } catch {
+                print("Error")
+            }
+            print(brand.objectForKey("brandTitle"))
+            cell.brandTitleLabel.text = brand.objectForKey("brandTitle") as? String
+            brand["brandImage"].getDataInBackgroundWithBlock { (imageData: NSData?, error:NSError?) -> Void in
+                if error == nil {
+                    let image = UIImage(data: imageData!)
+                    cell.brandImageView.image = image
+                }
+            }
+        
+        }
+        return cell
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
