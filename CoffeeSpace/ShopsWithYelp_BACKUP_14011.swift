e1707008c5238a21a93bc845026f6fcfd51c6519
lanista38 <jorge.cruz7@upr.edu>
2016-04-20 18:58:44 -0400

    there
    Signed-off-by: lanista38 <jorge.cruz7@upr.edu>

---
+++ CoffeeSpace/ShopsWithYelp_BACKUP_14011.swift
@@ -0,0 +1,116 @@
+//
+//  ShopsWithYelp.swift
+//  CoffeeSpace
+//
+//  Created by Jorge Cruz on 4/7/16.
+//  Copyright © 2016 Jorge Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ShopsWithYelp: NSObject {
+    let id: String?
+    let name: String?
+    
+    let address: String?
+    var coordinates: NSDictionary?
+    let imageURL: NSURL?
+    let categories: String?
+    let distance: String?
+    let ratingImageURL: NSURL?
+    let reviewCount: NSNumber?
+    
+    init(dictionary: NSDictionary) {
+        
+<<<<<<< Updated upstream
+        id = dictionary["id"] as? String
+=======
+        coordinates = nil
+>>>>>>> Stashed changes
+        name = dictionary["name"] as? String
+        
+        let imageURLString = dictionary["image_url"] as? String
+        if imageURLString != nil {
+            imageURL = NSURL(string: imageURLString!)!
+        } else {
+            imageURL = nil
+        }
+        
+        let location = dictionary["location"] as? NSDictionary
+        var address = ""
+        if location != nil {
+            let addressArray = location!["address"] as? NSArray
+            if addressArray != nil && addressArray!.count > 0 {
+                address = addressArray![0] as! String
+            }
+            let coordinatesDictionary = location!["coordinate"] as? NSDictionary
+            if(coordinatesDictionary != nil){
+                self.coordinates = coordinatesDictionary!
+            } else {
+                self.coordinates = nil
+            }
+            
+            let neighborhoods = location!["neighborhoods"] as? NSArray
+            if neighborhoods != nil && neighborhoods!.count > 0 {
+                if !address.isEmpty {
+                    address += ", "
+                }
+                address += neighborhoods![0] as! String
+            }
+        }
+        self.address = address
+        
+        let categoriesArray = dictionary["categories"] as? [[String]]
+        if categoriesArray != nil {
+            var categoryNames = [String]()
+            for category in categoriesArray! {
+                let categoryName = category[0]
+                categoryNames.append(categoryName)
+            }
+            categories = categoryNames.joinWithSeparator(", ")
+        } else {
+            categories = nil
+        }
+        
+        let distanceMeters = dictionary["distance"] as? NSNumber
+        if distanceMeters != nil {
+            let milesPerMeter = 0.000621371
+            distance = String(format: "%.2f mi", milesPerMeter * distanceMeters!.doubleValue)
+        } else {
+            distance = nil
+        }
+        
+        let ratingImageURLString = dictionary["rating_img_url_large"] as? String
+        if ratingImageURLString != nil {
+            ratingImageURL = NSURL(string: ratingImageURLString!)
+        } else {
+            ratingImageURL = nil
+        }
+        
+        reviewCount = dictionary["review_count"] as? NSNumber
+    }
+    
+    class func Shops(array array: [NSDictionary]) -> [ShopsWithYelp] {
+        var shops = [ShopsWithYelp]()
+        for dictionary in array {
+            let shop = ShopsWithYelp(dictionary: dictionary)
+            shops.append(shop)
+            
+            ShopLinkBrand.postShopBrandLink(shop.id!, brandId: ["000"], withCompletion: nil)
+        }
+        return shops
+        
+    }
+    
+    class func searchWithTerm(term: String, completion: ([ShopsWithYelp]!, NSError!) -> Void) {
+        Yelp_Client.sharedInstance.searchWithTerm(term, completion: completion)
+    }
+    
+   
+/**
+
+     //func searchWithTerm(term: String, sort: YelpSortMode?, categories: [String]?, deals: Bool?, completion: ([ShopsWithYelp]!, NSError!) -> Void) -> Void {
+        //Yelp_Client.sharedInstance.searchWithTerm(term, sort: sort, categories: categories, deals: deals, completion: completion)
+    //}
+ **/
+}
